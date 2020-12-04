10ecb26527c7a7b9b77d4c468f09aae29f853b5f
Scott Aiton <scott@aiton.net>
2020-12-04 07:44:27 -0700

    Fixed main class

--- src/main/java/dev/thunderegg/meshcreator/Main.java
+++ src/main/java/dev/thunderegg/meshcreator/Main.java
@@ -1,7 +1,11 @@
 package dev.thunderegg.meshcreator;
 
+/**
+ * Main class wrapper for javafx application, this serves as the entrypoint for
+ * the jar file
+ */
 public class Main {
     public static void main(String[] args) {
-        Main.main(args);
+        RMStart.main(args);
     }
 }
